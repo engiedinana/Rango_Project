33203fbb0c090f23419fc3eb89ae91c960919874
Sara Khalil <2606122K@student.gla.ac.uk>
2021-07-27 01:02:34 +0100

    added a static picture

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -27,6 +27,7 @@ SECRET_KEY = 'p_8*1ui0bek5)dyde$ai6p6o@3gtinvcv%e#q#^zxi3!dn#)4%'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR, ]
