096f88cffdbc6c487e1f03ad4c2a9ec1a814155c
Nikhita Sareen <nikhitasareen@gmail.com>
2021-08-02 19:36:53 +0100

    bug fix add page

--- rango/forms.py
+++ rango/forms.py
@@ -24,10 +24,8 @@ class PageForm(forms.ModelForm):
     def clean(self):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
-        # If url is not empty and doesn't start with 'http://',
-        # then prepend 'http://'.
-        if url and not url.startswith('http://'):
-            url = f'http://{url}'
+        if url and not (url.startswith('http://') or url.startswith('https://')):
+            url = f'https://{url}'
             cleaned_data['url'] = url
         return cleaned_data
     class Meta:
