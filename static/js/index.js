458ec562d0e1662576dcf587ba01f329ef320aea
Sara Khalil <2606122K@student.gla.ac.uk>
2021-08-04 23:05:45 +0100

    old version

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,36 @@
 window.addEventListener('load',  function() {
-   document.getElementById("greeting").innerHTML = "BAKRAHKO KOLOKO"
+   //document.getElementById("greeting").innerHTML = "BAKRAHKO KOLOKO"
    var xhr = new XMLHttpRequest();
    var k;
    xhr.onreadystatechange = function() {
       if (xhr.readyState == XMLHttpRequest.DONE) {
-         k = xhr.responseText
-          //alert(k);
-          //console.log(k)
+         var block_to_insert ;
+         var container_block ;
+         const k = JSON.parse(xhr.responseText);
+         var html=""
+         for(var i = 0; i < k.categories.length; i++){
+            console.log(k.categories[i].title)
+               html = html + `<div class="column">
+                        <div class="card">
+                           <img class = "img" src="/static/images/`+k.categories[i].image+`">
+                           <div class="container">
+                              <h2>` + k.categories[i].title + `</h2>
+                              <p class="title"> Rating: `+k.categories[i].rating+`</p>
+                              <ul>`
+            for(var j = 0; j < k.categories[i].pages.length; j++){
+               console.log(k.categories[i].pages[j])
+               html = html + `<li>
+                                 <a href=` + k.categories[i].pages[j].url +`>`+k.categories[i].pages[j].title+`</a>
+                              </li>`
+            }
+            html = html + `</ul> 
+                        </div> 
+                     </div>
+                  </div>`
+         }
+         document.getElementById('row').innerHTML = html
       }
   }
    xhr.open("GET", "get_cat", true);
    xhr.send(null);
-   
 });
