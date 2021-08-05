8a5b07ccad761fd73affb11447bb7948af7fed2f
Sara Khalil <2606122K@student.gla.ac.uk>
2021-08-05 14:49:44 +0100

    alphabets navigation bar UI done

--- static/js/index.js
+++ static/js/index.js
@@ -26,40 +26,16 @@ window.addEventListener('load',  function() {
   }
    xhr.open("GET", "/rango/get_cat", true);
    xhr.send(null);
+   
+$('.AlphabetNav a').click(function(evt) {
+   evt.preventDefault();
+   var $navItem = $(this)
+   
+   if ($navItem.hasClass('active')) {
+     $navItem.removeClass('active');
+   } else {
+    $('.AlphabetNav a').removeClass('active');
+     $navItem.addClass('active');
+       }
+ }); 
 });
-let createArrayAtoZ = _ => {
-   return Array 
-      .apply(null, {length: 26}) 
-      .map((x, i) => String.fromCharCode(65 + i));
-}
-
-let jumptoAnchor = anchor => {
-   window.location.href = "#" + anchor;
-}
-
-let createNavigationList = _ => {
-   const abcChars = createArrayAtoZ();
-   const navigationEntries = abcChars.reduce(createDivForCharElement, ''); 
-   $('#nav').append(navigationEntries);
-
-   const lettersActive = createArrayAtoZ();
-   lettersActive.forEach(letter => { 
-      changeItemState(letter); 
-      addListEntries(letter); 
-   });
-}
-
-let changeItemState = character => {
-   const characterElement = $('#nav').find('.CharacterElement[data-filter="' + character + '"]');
-   $(characterElement).click(() => jumptoAnchor(character));
-   characterElement.removeClass('Inactive');
-}
-
-let createDivForCharElement = (block, charToAdd) => { 
-   return block + "<div id='CharacterElement' class='CharacterElement Inactive' data-filter='" + charToAdd + "'>" + charToAdd + "</div>"; 
-}
-
-let addListEntries = letter => {
-   $('#AppComponent').append("<div class='AppElement' id='" + letter + "'>" + letter + "</div>");
-}
-
