969195776a631167ab62bcc685beb6037a65ec53
Sara Khalil <2606122K@student.gla.ac.uk>
2021-08-05 02:45:29 +0100

    first trial for letter filtering

--- static/js/index.js
+++ static/js/index.js
@@ -29,3 +29,39 @@ window.addEventListener('load',  function() {
    xhr.open("GET", "/rango/get_cat", true);
    xhr.send(null);
 });
+let createArrayAtoZ = _ => {
+   return Array 
+      .apply(null, {length: 26}) 
+      .map((x, i) => String.fromCharCode(65 + i));
+}
+
+let jumptoAnchor = anchor => {
+   window.location.href = "#" + anchor;
+}
+
+let createNavigationList = _ => {
+   const abcChars = createArrayAtoZ();
+   const navigationEntries = abcChars.reduce(createDivForCharElement, ''); 
+   $('#nav').append(navigationEntries);
+
+   const lettersActive = createArrayAtoZ();
+   lettersActive.forEach(letter => { 
+      changeItemState(letter); 
+      addListEntries(letter); 
+   });
+}
+
+let changeItemState = character => {
+   const characterElement = $('#nav').find('.CharacterElement[data-filter="' + character + '"]');
+   $(characterElement).click(() => jumptoAnchor(character));
+   characterElement.removeClass('Inactive');
+}
+
+let createDivForCharElement = (block, charToAdd) => { 
+   return block + "<div id='CharacterElement' class='CharacterElement Inactive' data-filter='" + charToAdd + "'>" + charToAdd + "</div>"; 
+}
+
+let addListEntries = letter => {
+   $('#AppComponent').append("<div class='AppElement' id='" + letter + "'>" + letter + "</div>");
+}
+
