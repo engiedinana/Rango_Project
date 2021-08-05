17affaa39db3f5e619a77c0e88b7bf0a8d47b601
Sara Khalil <2606122K@student.gla.ac.uk>
2021-08-06 00:08:31 +0100

    rating in category in progress

--- static/js/category.js
+++ static/js/category.js
@@ -1,3 +1,28 @@
-window.addEventListener('load',  function() {
-    document.getElementById("image").src = "static/images/"+document.getElementById("myBtn").textContent;
+ window.addEventListener('load',  function() {
+    var star = ""
+    var directories = $(location).attr('href').split("/");
+    var category =  directories[directories.length - 2];
+   $("#star1").click(function(){
+       star = $("#star1").val()
+});
+
+$("#star2").click(function(){
+    star = $("#star2").val()
+});
+$("#star3").click(function(){
+    star = $("#star3").val()
+});
+$("#star4").click(function(){
+    star = $("#star4").val()
+});
+$("#star5").click(function(){
+    star = $("#star5").val()
+});
+
+
+
+
+
+
+
 });
