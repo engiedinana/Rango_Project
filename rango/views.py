cded9e5927e36e42bbe8216133f7218829cad5c0
Engie Dinana <2517261D@student.gla.ac.uk>
2021-08-06 14:37:19 +0100

    view.py comments done

--- rango/views.py
+++ rango/views.py
@@ -179,7 +179,11 @@ def get_server_side_cookie(request, cookie, default_val=None):
         val = default_val
     return val
 
-# Updated the function definition
+"""
+Description: This is a helper function to retrieve handle visitor cookie
+Params: request
+Return: visits
+"""
 def visitor_cookie_handler(request):
     visits = int(get_server_side_cookie(request, 'visits', '1'))
     last_visit_cookie = get_server_side_cookie(request,'last_visit',str(datetime.now()))
@@ -195,8 +199,6 @@ def visitor_cookie_handler(request):
     # Update/set the visits cookie
     request.session['visits'] = visits
 
-
-
 def index(request):
     # Query the database for a list of ALL categories currently stored.
     # Order the categories by the number of likes in descending order.
@@ -204,16 +206,21 @@ def index(request):
     # Place the list in our context_dict dictionary (with our boldmessage!)
     # that will be passed to the template engine.
     context_dict = add_cat_supcat_pages_context()
-    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     # Render the response and send it back!
     visitor_cookie_handler(request)
     response = render(request, 'rango/index.html', context=context_dict)
     return response
 
+"""
+Description: This function prepare the data for the homepage
+Params: request
+Return: json dump for contect dictionary to be utilized by java script
+"""
 def get_cat(request):
     context_dict = {}
     category_list = Category.objects.all().order_by('-rating')
     listOfCat = []
+    # get categories in the system to display in homepage
     for category in category_list:
         page_list = Page.objects.all().filter(category_id=category.id)[:3]
         cat = {}
@@ -223,6 +230,7 @@ def get_cat(request):
         cat["image"]=str(category.image)
         cat["last_modified"]=str(category.last_modified)
         listOfPag = []
+        # get their associated pages to show in the homepage
         for page in page_list:
             pag = {}
             pag["title"]= page.title
@@ -238,31 +246,39 @@ def about(request):
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    context_dict = add_cat_supcat_pages_context()
+    context_dict = add_cat_supcat_pages_context() # to show everything in the navigation bar
     visitor_cookie_handler(request)
     context_dict['visits'] = request.session['visits']
     response = render(request, 'rango/about.html', context=context_dict)
     return response
 
+"""
+Description: This function redirects to the terms of use page
+Params: request
+Return: response
+"""
 def terms_of_use(request):
-    context_dict = add_cat_supcat_pages_context()
+    context_dict = add_cat_supcat_pages_context() # to show everything in the navigation bar
     response = render(request, 'rango/terms_of_use.html', context_dict)
     return response
-
-# Create your views here.
+"""
+Description: This function shows the details of the category
+Params: request, category_name_slug
+Return: render category page
+"""
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
     # to the template rendering engine.
-    context_dict = add_cat_supcat_pages_context()
+    context_dict = add_cat_supcat_pages_context() # to add everything to the navigation bar
     current_user = get_user(request)
-
+    #make sure the category exists
     try:
         category = Category.objects.get(slug=category_name_slug)
     except Category.DoesNotExist:
         category = None
     if category == None:
         return redirect(reverse('rango:index'))
-    
+    # check if the user is registered or not
     try:
         if (not current_user.is_anonymous) and (current_user is not None):
             profile = UserProfile.objects.get(user=current_user)
@@ -286,7 +302,7 @@ def show_category(request, category_name_slug):
         # We'll use this in the template to verify that the category exists.
         context_dict['category'] = category
         context_dict['comments'] = comment_list
-         #Get user instance
+        #Get user instance
         user = current_user
         if (not user.is_anonymous) and (user is not None):
             #Get all the favorite pages of the user and send to render on page
@@ -319,19 +335,25 @@ def show_category(request, category_name_slug):
         #It is not a post request, it is a get request and render the comment form to the user
     return render(request, 'rango/category.html', context=context_dict)
 
+"""
+Description: This function adds a new category to the db associated with an existing super category
+Params: request, username
+Return: renders homepage
+"""
 @login_required
 def add_category(request, username):
-    context_dict = add_cat_supcat_pages_context()
+    context_dict = add_cat_supcat_pages_context() # to show everything in the navigation bar
     form = CategoryForm()
     # A HTTP POST?
     if request.method == 'POST':
         form = CategoryForm(request.POST)
         mytitle = request.POST.get('title')
+        #  get the super category that it would belong to
         try:
             super_category = SuperCategories.objects.get(pk = mytitle)
         except SuperCategories.DoesNotExist:
             super_category = None
-
+        # ge tthe user who created the category
         try:
             user = User.objects.get(username = username)
         except SuperCategories.DoesNotExist:
@@ -344,7 +366,7 @@ def add_category(request, username):
         if form.is_valid():
             # Save the new category to the database.
             category = form.save(commit=False)
-            if 'image' in request.FILES:
+            if 'image' in request.FILES: # save category image if any
                 category.image = request.FILES.get('image')
             category.super_cat = super_category
             # category.user = user
@@ -361,46 +383,57 @@ def add_category(request, username):
     context_dict['form'] = form
     return render(request, 'rango/add_category.html', context_dict)
 
+"""
+Description: This function adjusts the rating of a category in the db based on the incoming value
+Params: request, slug
+Return: response
+"""
 def rate_category(request, category_name_slug,star):
+    # get category that is being rated
     try:
         category = Category.objects.get(slug=category_name_slug)
     except Category.DoesNotExist:
         category = None
         return HttpResponse("Failed to get") 
-    # You cannot add a page to a Category that does not exist...
+    # update the sum and count to be able to calculate the avergae rating so far
     sum = category.rating_sum_val
     count = category.rating_count_val 
+    # update database rating for this category
     if request.method == 'GET':
         category.rating_sum_val = sum + int(star)
         category.rating_count_val = count + 1
-        print(category.rating_count_val)
+        msg = ""
         try:
             value  = math.floor((sum + int(star)) / (count + 1))
             if (value>5):
                 category.rating  = 5
+                msg = "Rating cannot exceed 5"
             else:
                 category.rating = math.floor(category.rating_sum_val / category.rating_count_val) #divide by zero!
+                msg = "Success"
         except:
-            if (sum>5):
-                category.rating  = 5
-                # return HttpResponse("rating greater than five, adjusted to equal 5")
-            else:
-                category.rating = category.rating_sum_val
+            category.rating = category.rating_sum_val
+            msg = "Divide by zero exception"
         category.save()
-    return HttpResponse("success")
-   
+        return HttpResponse(msg)
+
+"""
+Description: This function allows users to add pages to an existing category; user must be logged in
+Params: request, slug
+Return: renders the category page
+"""  
 @login_required
 def add_page(request, category_name_slug):
+    # get category to which the page would be added
     try:
         category = Category.objects.get(slug=category_name_slug)
     except Category.DoesNotExist:
         category = None
 
-    # You cannot add a page to a Category that does not exist...
     if category is None:
         return redirect('/rango/')
     form = PageForm()
-
+    # update the pages table to have the new link associated with a category
     if request.method == 'POST':
         form = PageForm(request.POST)
         if form.is_valid():
@@ -417,6 +450,11 @@ def add_page(request, category_name_slug):
     context_dict['category'] = category
     return render(request, 'rango/add_page.html', context=context_dict)
 
+"""
+Description: This function adds a new user to the system
+Params: request
+Return: render the registration page
+"""
 def register(request):
     # A boolean value for telling the template
     # whether the registration was successful.
@@ -452,7 +490,7 @@ def register(request):
             # Did the user provide a profile picture?
             # If so, we need to get it from the input form and
             #put it in the UserProfile model.
-            if 'picture' in request.FILES:
+            if 'picture' in request.FILES: #allows user to upload a picture
                 profile.picture = request.FILES['picture']
 
             # Now we save the UserProfile model instance.
@@ -477,6 +515,11 @@ def register(request):
     # Render the template depending on the context.
     return render(request,'rango/register.html',context = context_dict)
 
+"""
+Description: This function allows uses to login to the system using their saved credentials
+Params: request
+Return: render the login page
+"""
 def user_login(request):
     # If the request is a HTTP POST, try to pull out the relevant information.
     context_dict = add_cat_supcat_pages_context()
@@ -518,6 +561,11 @@ def user_login(request):
         # blank dictionary object...
         return render(request, 'rango/login.html', context_dict)
 
+"""
+Description: This function allows users to logout from the system
+Params: request
+Return: redirects to homepage
+"""
 #Use the login_required() decorator to ensure only those logged in can
 # access the view.
 @login_required
@@ -529,6 +577,11 @@ def user_logout(request):
 
 #View for saving a favorite page
 
+"""
+Description: This function allows users to save pages for future reference
+Params: view
+Return: JSON response
+"""
 class SaveFavoriteView(View):
     #only authenticated users can add a favorite page
     @method_decorator(login_required)
@@ -550,9 +603,11 @@ class SaveFavoriteView(View):
         #return response 
         return JsonResponse({'response' : 1})
     
-    #def get(self, request):
-     #   return render(request, 'rango/category.html') 
-     
+"""
+Description: This function allows users to unsave pages
+Params: view
+Return: JSON response
+"""    
 #View for removing a page from favorites
 class UnsaveFavoriteView(View):
     #only authenticated users can add a favorite page
@@ -574,10 +629,15 @@ class UnsaveFavoriteView(View):
         to_remove = user.pages.all().filter(id=page_id)
         user.pages.remove(to_remove[0])
         user.save()
-        #return response
         return JsonResponse({'response' : 1})
 
+"""
+Description: This class allows users to view their profile details and saved pages
+Params: view
+Return: renders profile page
+"""
 class ProfileView(View):
+    # helper function to retrieve user details
     def get_user_details(self, username):
         try:
             user = User.objects.get(username=username)
@@ -588,6 +648,7 @@ class ProfileView(View):
         user_profile = UserProfile.objects.get_or_create(user=user)[0]
         form = UserProfileForm({'website': user_profile.website, 'picture':user_profile.picture})
         return (user, user_profile, form)
+    # helper function to get the saved links of the user
     @method_decorator(login_required)
     def get(self, request, username):
         #Render saved list 
@@ -602,7 +663,7 @@ class ProfileView(View):
         context_dict['form'] = form
         context_dict['pages']=fav_list
         return render(request, 'rango/profile.html', context_dict)
-    
+    # renders the profile page
     @method_decorator(login_required)
     def post(self, request, username):
         context_dict = add_cat_supcat_pages_context()
@@ -622,7 +683,12 @@ class ProfileView(View):
         context_dict['form']=form
         return render(request, 'rango/profile.html', context_dict)
 
-#View for the contact us form
+
+"""
+Description: This class allows users to submit enquiries or problems
+Params: view
+Return: renders contact us page
+"""
 class ContactUsView(View):
     #get request to show user the empty form
     def get(self,request):
@@ -647,14 +713,23 @@ class ContactUsView(View):
         context_dict['form'] = form
         return render(request, 'rango/contact_us.html', {'form': form})
 
+
+"""
+Description: This function allows users to reset their password
+Params: request
+Return: renders reset_password page
+"""
 def password_reset_request(request):
-    context_dict = add_cat_supcat_pages_context()
+    context_dict = add_cat_supcat_pages_context() #used for the navigation bar
     if request.method == "POST":
         password_reset_form = PasswordResetForm(request.POST)
         if password_reset_form.is_valid():
+            # get the email to which we will send a reset password link
             data = password_reset_form.cleaned_data['email']
+            # get user from the DB
             associated_users = User.objects.filter(Q(email=data))
             if associated_users.exists():
+                # construct email that will appear in the terminal with the reset link
                 for user in associated_users:
                     subject = "Password Reset Requested"
                     email_template_name = "rango/password/password_reset_email.txt"
@@ -669,10 +744,12 @@ def password_reset_request(request):
                     }
                     email = render_to_string(email_template_name, c)
                     try:
+                        # show the email in the terminal
                         send_mail(subject, email, 'admin@example.com', [user.email], fail_silently=False)
                     except BadHeaderError:
                         return HttpResponse('Invalid header found.')
                     return redirect("done/")
+    # render the reset password page
     password_reset_form = PasswordResetForm()
     context_dict["password_reset_form"] =  password_reset_form
     return render(request=request, template_name="rango/password/password_reset.html",
