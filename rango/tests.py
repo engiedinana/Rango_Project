7fa7686729f137272aeb91c90b122814249eb14f
Sara Khalil <2606122K@student.gla.ac.uk>
2021-08-06 06:24:30 +0100

    home page tests

--- rango/tests.py
+++ rango/tests.py
@@ -5,6 +5,7 @@ from rango.models import Page, Category, SuperCategories
 from django.contrib.auth import get_user_model
 #from django.test import Client
 import json
+import datetime
 
 #helper function to populat database
 def create_user(username, password):
@@ -14,10 +15,11 @@ def create_user(username, password):
     return user
 
 #helper function to populat database
-def create_category(name, super, user):
+def create_category(name, super, user,date):
     category, __ = Category.objects.update_or_create(
         name=name,
-        defaults={'super_cat': super}
+        defaults={'super_cat': super},
+        last_modified = date
     )
     return category
 
@@ -42,7 +44,7 @@ class TestFavoritesFeature(TestCase):
     def setUp(self):
         self.user = create_user("testUser", "testPassword")
         self.super = create_super_category('Frameworks')
-        self.cat = create_category('python', self.super, self.user)
+        self.cat = create_category('python', self.super, self.user,datetime.datetime.now().date())
         self.page= add_page('learn x in y minutes','https://learnxinyminutes.com/docs/python', self.cat,self.user)        
         
     def tearDown(self):
@@ -91,5 +93,56 @@ class TestFavoritesFeature(TestCase):
     def test_database_get_saved_and_save_unsaved_pages(self):
         pass
 """
-# class TestRatingFeature(TestCase):
-    
+class TestRatingFeature(TestCase):
+    @classmethod
+    def setUpTestData(self):
+        self.user = create_user("testUser", "testPassword")
+        self.super = create_super_category('Frameworks')
+        self.cat = create_category('python', self.super, self.user, datetime.datetime.now().date())
+        self.page= add_page('learn x in y minutes','https://learnxinyminutes.com/docs/python', self.cat,self.user) 
+
+    # def test_rating_view(self):
+    #     self.response = self.client.get(reverse('rango:get', kwargs={'category_name_slug': 'python'}))
+    #     self.content = self.response.content.decode()
+
+
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/rango/get_cat/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('rango:get_cat', kwargs={}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/rango/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('rango:index', kwargs={}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('rango:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'rango/index.html')
+
+
+
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/rango/get_cat/')
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {"categories": [{"title": "python", "slug": "python", "rating": 0, "image": "", "last_modified":datetime.datetime.now().date().strftime('%Y-%m-%d'), "pages": [{"title": "learn x in y minutes", "url":"https://learnxinyminutes.com/docs/python", "description": ""}]}]})
+        # self.assertJSONEqual(
+        #     str(response.content, encoding='utf8'),
+        #     {'status': 'success'}
+        # )
+    # def test_lists_all_authors(self):
+    #     # Get second page and confirm it has (exactly) remaining 3 items
+    #     response = self.client.get(reverse('index'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTrue('is_paginated' in response.context)
+    #     self.assertTrue(response.context['is_paginated'] == True)
+    #     self.assertEqual(len(response.context['author_list']), 3)
